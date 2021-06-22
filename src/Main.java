8ccefb6310a0bc410a1c06a715d1a321749911bd
Elienock <lubayaelienock@gmail.com>
2021-06-22 14:45:40 +0200

    updated version

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// declare variables
+        int num1 ,num2,sum;
+        boolean doItAgain=true;
+
+        do{
+            //get the two numbers
+            try {
+                num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter the first number : "));
+                num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter the second number : "));
+
+                //determine the sum
+                sum = num1 + num2;
+
+                //display the sum
+                JOptionPane.showMessageDialog(null, "The sum of " + num1 +" and " + num2 + " is " + sum);
+                 doItAgain=false;
+            }catch(NumberFormatException nfe){
+                JOptionPane.showMessageDialog(null,"You have entered the wrong input. please enter a number");
+            }
+        }while(doItAgain);
     }
 }
